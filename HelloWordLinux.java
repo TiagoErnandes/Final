74b0ffd8d9cffef9e1b3d07012d4d9269622200f
TiagoErnandes <tiagoestevam7@hotmail.com>
2019-06-11 19:19:09 -0300

    Matriz11.06

--- HelloWordLinux.java
+++ HelloWordLinux.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+
 public class HelloWordLinux {
     public static void main(String[] args) {
-        System.out.println("Hello, Word!");
+        Random rand = new Random();
+        for (int i = 0; i < 20; i++) {
+            System.out.println(rand.nextInt(10)+1);
+        }
     }
 }
